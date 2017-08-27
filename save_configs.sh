968f0111b7bf1d6da592715ed630888734fd59fa
Ladislao <vladX09rus@gmail.com>
2017-08-27 20:20:14 +0300

    Add udev rule. Update some scripts

--- save_configs.sh
+++ save_configs.sh
@@ -1,7 +1,7 @@
-HOME=/home/vlad123
-DIR=$HOME/configs
-mkdir $DIR
+HOME=~
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $HOME/.xmonad/xmonad.hs $DIR/
 cp $HOME/.Xresources $DIR/
 cp $HOME/.conkyrc $DIR/ 
 cp -r $HOME/.local/scripts/ $DIR/
+cp /etc/udev/rules.d/95-myudev.rules $DIR/
---
+++ scripts/displayconf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/vlad/.Xauthority
+
+HDMIs=$(xrandr | grep HDMI | grep -w connected |wc -l)
+VGAs=$(xrandr | grep VGA  | grep -w connected |wc -l)
+
+echo "$dev" >> /home/vlad/debug.log
+case $dev in
+0)
+	xrandr --output LVDS-1 --primary
+	xrandr --output HDMI-1 --off
+	xrandr --output VGA-1  --off
+;;
+1)
+	xrandr --output VGA-1 --auto --left-of LVDS-1 --primary
+;;
+10)
+	xrandr --output HDMI-1 --auto --left-of LVDS-1 --primary
+;;
+11)
+	xrandr --output VGA-1 --auto --left of LVDS-1
+	xrandr --output HDMI-1 --auto --left-of VGA-1 --primary
+;;
+esac
+
+pkill dzen2
+xmonad --restart
