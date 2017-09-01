e38adf32ccdbeebc3ca0de3e1fb574b1377af0c1
Ladislao <vladX09rus@gmail.com>
2017-09-01 17:53:20 +0300

    Add GTK Arc theme and Papirus icons configs

--- save_configs.sh
+++ save_configs.sh
@@ -5,5 +5,7 @@ cp $HOME/.Xresources $DIR/
 cp $HOME/.conkyrc $DIR/ 
 cp -r $HOME/.local/scripts/ $DIR/
 cp /etc/udev/rules.d/95-myudev.rules $DIR/
-cp -r /etc/lightdm/lightdm.conf.d/ $DIR/ 
+cp -r /etc/lightdm/ $DIR/ 
+cp -r $HOME/.config/gtk-3.0/settings.ini $DIR/
+cp $HOME/.gtkrc-2.0 $DIR/
 
