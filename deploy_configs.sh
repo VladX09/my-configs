5a4dd530c4679b33cf2c199d92e63b8c7563d16f
Ladislao <vladX09rus@gmail.com>
2018-03-08 00:37:29 +0300

    Add symlink-based deploy script

---
+++ deploy_configs.sh
@@ -0,0 +1,23 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+XRESOURCES=~/.Xresources
+SCRIPTS=~/.local/scripts
+LIGHTDM=/etc/lightdm
+GTK3_CONFIG=~/.config/gtk-3.0/settings.ini
+GTK2_CONFIG=~/.gtkrc-2.0
+BSPWM=~/.config/bspwm
+SXHKD=~/.config/sxhkd
+POLYBAR=~/.config/polybar
+AWESOME=/usr/share/fonts/opentype/awesome
+COMPTON=~/.config/compton.conf
+
+ln -s $DIR/.Xresources $XRESOURCES
+ln -s $DIR/scripts $SCRIPTS
+sudo ln -s $DIR/lightdm $LIGHTDM
+ln -s $DIR/settings.ini $GTK3_CONFIG
+ln -s $DIR/.gtkrc-2.0 $GTK2_CONFIG
+ln -s $DIR/bspwm $BSPWM
+ln -s $DIR/sxhkd $SXHKD
+ln -s $DIR/polybar $POLYBAR
+sudo ln -s $DIR/awesome $AWESOME
+ln -s $DIR/compton.conf $COMPTON
+
