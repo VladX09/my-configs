3f40701c91ecaa06c21865846d558d9037c64d36
Vladislav Verba <v.verba@iva-tech.ru>
2019-02-26 12:08:17 +0300

    Add orgmode

--- .emacs.d.exp/init.el
+++ .emacs.d.exp/init.el
@@ -29,8 +29,9 @@
 (load-user-file "keyboard.el")
 (load-user-file "helm-conf.el")
 (load-user-file "theme.el")
-;; (load-user-file "python-conf.el")
-(load-user-file "python-ls.el")
+(load-user-file "python-conf.el")
+(load-user-file "org.el")
+;; (load-user-file "python-ls.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
